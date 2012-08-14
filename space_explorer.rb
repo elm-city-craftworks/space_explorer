72c51a4c1e52512de39d6e804ca306020c7e956a
Gregory Brown <gregory.t.brown@gmail.com>
2012-08-14 14:03:34 -0400

    Doing maps wrong. Figure out how to do them right

--- space_explorer.rb
+++ space_explorer.rb
@@ -1,6 +1,32 @@
 require "thread"
 
 module SpaceExplorer
+
+  class World
+    def initialize(observer)
+      @observer  = observer
+
+      @data      = [[:empty, :empty, :empty, :empty],
+                    [:rock,  :empty, :empty, :empty],
+                    [:rock,  :empty, :empty, :empty],
+                    [:rock,  :rock,  :empty, :empty]]
+    end
+
+    def snapshot(reference_point)
+      ref_col, ref_row = reference_point
+
+      snapshot = [-1,0,1].product([-1,0,1]).map do |col, row|
+        if col == 0 && row == 0
+          :rover
+        else
+          @data[ref_col + col][ref_row + row]
+        end
+      end
+
+      @observer.snapshot = snapshot.each_slice(3).to_a
+    end
+  end
+
   class MissionControl
     def initialize(narrator)
       @narrator = narrator
@@ -20,9 +46,14 @@ module SpaceExplorer
 
   class Rover
     attr_writer :radio_link
+    attr_writer :snapshot
 
     def initialize
       @queue = Queue.new
+      @world = World.new(self)
+
+      @column = 2
+      @row    = 2
     end
 
     def receive_command(command)
@@ -33,13 +64,22 @@ module SpaceExplorer
       case command
       when "PING"
         @radio_link.transmit("PONG")
+      when "NORTH"
+        @row -= 1
+      when "SOUTH"
+        @row += 1
+      when "EAST"
+        @column += 1
+      when "WEST"
+        @column -= 1
       when "SNAPSHOT"
-        map = ["X - - - -",
-               "X - - - -",
-               "X X O - -",
-               "- X - - X",
-               "- - X X -"].join("\n")
-        @radio_link.transmit("\n#{map}")
+        @world.snapshot([@row, @column])
+
+        output = @snapshot.map { |row|
+          row.map { |e| { :empty => "-", :rock => "X", :rover => "O" }[e] }.join(" ")
+        }.join("\n")
+
+        @radio_link.transmit("\n#{output}")
       else
         @radio_link.transmit("NOT IMPLEMENTED: #{command}")
       end
@@ -53,7 +93,7 @@ module SpaceExplorer
   end
 
   class Radio
-    TRANSMISSION_DELAY = 30
+    TRANSMISSION_DELAY = 1
 
     def initialize(target)
       @target = target
